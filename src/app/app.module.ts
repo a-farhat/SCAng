4f3f18f3149235b25b192033ed1448efcb480485
a-farhat <akfarhat@gmail.com>
2021-02-27 11:20:07 +0200

    Added new Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+//Main App Component
 import { AppComponent } from './app.component';
+
+//Custom Components
+import { IResumeComponent } from './iresume/iresume.component';
+import { AboutComponent } from './About/about.component';
 import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    IResumeComponent,
+    AboutComponent,
     HomeComponent
+
   ],
   imports: [
     BrowserModule,
